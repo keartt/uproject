526b114fd874213ae932d6614f0e50eea5fcc401
Default <shalpha_2@naver.com>
2022-10-28 00:26:38 +0900

    12 List<Entity>

--- src/main/java/com/example/uproject/controller/ArticleController.java
+++ src/main/java/com/example/uproject/controller/ArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ArticleController {
@@ -55,4 +57,19 @@ public class ArticleController {
         //3 보여줄 페이지를 설정한다.
         return "articles/show";
     }
+    @GetMapping("/articles")
+    public String index(Model model){
+
+        //1 모든 Article 을 가져온다
+        List<Article> articleEntityList = articleRepository.findAll();
+        // repository 에 ArrayList 오버라이딩 필요
+
+        //2 가져온 Article 묶음을 뷰로 전달
+        model.addAttribute("articleList", articleEntityList);
+
+        //뷰 페이지를 설계한다
+        return "articles/index"; // articles/index.mustache
+
+    }
+
 }
--- src/main/java/com/example/uproject/repository/ArticleRepository.java
+++ src/main/java/com/example/uproject/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.uproject.repository;
 import com.example.uproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
